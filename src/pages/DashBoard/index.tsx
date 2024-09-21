686337517e9fc0372e35005e10c055b44f8bc773
Bryan K <2455350756@qq.com>
2024-09-21 17:53:42 -0400

    Dashboard基本布局框架

--- src/pages/DashBoard/index.tsx
+++ src/pages/DashBoard/index.tsx
@@ -1,8 +1,25 @@
+import '@Dashboard/Dashboard.css'
+import { useState } from 'react';
+
+let [sidepanel, setSidepanel] = useState(false);
+function toggleSidepanel() {
+  setSidepanel(!sidepanel);
+}
 function Dashboard() {
   return (
     <div className="dashboard">
-      <h1>Dashboard</h1>
-      <p>Welcome to the dashboard!</p>
+      <div className="dashboard-header">
+        <button onClick={toggleSidepanel}>Toggle Side Panel</button>
+        <h1>Dashboard</h1>
+      </div>
+      {sidepanel && (
+        <div className="dashboard-sidepanel">
+        <h2>Side Panel</h2>
+      </div>
+      )}
+      <div className="dashboard-content">
+        <p>Welcome to the dashboard!</p>
+      </div>
     </div>
   );
 }
