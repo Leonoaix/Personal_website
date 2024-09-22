d5d95598bb9cf0c70242a1deae28cc9fd9f32057
Bryan K <2455350756@qq.com>
2024-09-21 22:52:11 -0400

    Bug修复
    文档流修复

--- src/pages/DashBoard/index.tsx
+++ src/pages/DashBoard/index.tsx
@@ -1,24 +1,31 @@
-import '@Dashboard/Dashboard.css'
+import './Dashboard.css'
 import { useState } from 'react';
 
-let [sidepanel, setSidepanel] = useState(false);
-function toggleSidepanel() {
-  setSidepanel(!sidepanel);
-}
 function Dashboard() {
+  const [sidepanel, setSidepanel] = useState(false);
+
+  function toggleSidepanel() {
+    setSidepanel(!sidepanel);
+  }
+
   return (
     <div className="dashboard">
       <div className="dashboard-header">
-        <button onClick={toggleSidepanel}>Toggle Side Panel</button>
+        <button onClick={toggleSidepanel} className="Toggle-Side-Panel"></button>
         <h1>Dashboard</h1>
       </div>
-      {sidepanel && (
-        <div className="dashboard-sidepanel">
-        <h2>Side Panel</h2>
-      </div>
-      )}
-      <div className="dashboard-content">
-        <p>Welcome to the dashboard!</p>
+      <div className="dashboard-main-content">
+        {sidepanel && (
+          <div>
+            <div className="sidepanel-placeholder"></div>
+            <div className="dashboard-sidepanel">
+              <h2>Side Panel</h2>
+            </div>
+          </div>
+        )}
+        <div className="dashboard-content">
+          <p>Welcome to the dashboard!</p>
+        </div>
       </div>
     </div>
   );
